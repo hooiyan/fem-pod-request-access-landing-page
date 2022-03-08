b6d10e4d64fdeeb4ef7a0abb94f63fc5af1fb241
hooiyan <hoecoding@gmail.com>
2022-03-08 18:57:12 +0800

    Add simple javascript form validation

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 const form = document.querySelector('form');
 const input = document.querySelector('form input')
-const submitButton = document.querySelector('form button')
+const formError = document.getElementById('form-error')
+
+// Receive an error message when the form is submitted if:
+  // 1. The Email address field is empty should show "Oops! Please add your email"
+  // 2. The email is not formatted correctly should show "Oops! Please check your email"
+
+  form.addEventListener('submit', e => { 
+    e.preventDefault()
+
+    if (input.value === '') {
+      formError.innerText = 'Oops! Please add your email'
+      formError.classList.remove('inactive')
+      formError.classList.add('error')
+    } else if (input.value.indexOf('@') === -1) {
+      formError.innerText = 'Oops! Please check your email'
+      formError.classList.remove('inactive')
+      formError.classList.add('error')
+    } else {
+      formError.classList.add('inactive')
+      formError.classList.remove('error')
+    }
+  })
